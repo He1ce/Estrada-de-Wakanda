84724c31ee7f962e2bf14f7183a069e5d3e8707f
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-04-17 17:56:06 -0300

    função cidadeMenorVizinhanca implementada

--- cidades.c
+++ cidades.c
@@ -5,47 +5,64 @@
 
 #include "cidades.h"
 
+void ordenarCidadesPorPosicao(Estrada *E) {
+    int i, j;
+    for (i = 0; i < E->N - 1; i++) {
+        for (j = 0; j < E->N - i - 1; j++) {
+            if (E->C[j].Posicao > E->C[j + 1].Posicao) {
+                Cidade temp = E->C[j];
+                E->C[j] = E->C[j + 1];
+                E->C[j + 1] = temp;
+            }
+        }
+    }
+}
+
 Estrada *getEstrada(const char *nomeArquivo){
 
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL) {
         printf("ERRO! Falha ao abrir o arquivo.\n");
+        fclose(arquivo);
         return NULL;
     }
 
     Estrada *E = (Estrada *)malloc(sizeof(Estrada));
     if (E == NULL){
         printf("Erro ao criar E\n");
+        fclose(arquivo);
         return NULL;
     }
 
 
     fscanf(arquivo, " %d", &E->T);
     if(E->T < 3 || E->T > pow(10,6)){
-        printf("ERRO! Valor de T invalido.\n");
+        printf("ERRO! Valor de E invalido.\n");
+        fclose(arquivo);
         return NULL;
     }
 
     fscanf(arquivo, " %d", &E->N);
     if(E->T < 2 || E->T > pow(10,4)){
         printf("ERRO! Valor de N invalido.\n");
+        fclose(arquivo);
         return NULL;
     }
       
 
     E->C = (Cidade *)malloc(E->N * sizeof(Cidade));
     if (E->C == NULL){
-        printf("Erro: não foi possível alocar memória para a lista\n");
-        free(arquivo);
+        printf("Erro: nao foi possi�vel alocar memoria para a lista\n");
+        fclose(arquivo);
         return NULL;
     }
 
 
     int i = 0;
     while (fscanf(arquivo, " %d %[^\n]\n", &E->C[i].Posicao, E->C[i].Nome) == 2){
-   
         if(E->C[i].Posicao <= 0 || E->C[i].Posicao >= E->T){
             printf("ERRO! Posicao %d invalida.\n", E->C[i].Posicao);
+            fclose(arquivo);
             return NULL;
         }
         i++;
@@ -56,11 +73,13 @@ Estrada *getEstrada(const char *nomeArquivo){
         for(j = 0; j < i; j++){
             if(E->C[i].Posicao == E->C[j].Posicao){
                 printf("ERRO! Ja ha uma cidade na posicao %d.\n", E->C[i].Posicao);
+                fclose(arquivo);
                 return NULL;
             }
         }
     }
 
+    ordenarCidadesPorPosicao(E);
 
     fclose(arquivo);
     return E;
@@ -68,28 +87,28 @@ Estrada *getEstrada(const char *nomeArquivo){
 
 double calcularMenorVizinhanca(const char *nomeArquivo){
 
-    Estrada *T = getEstrada(nomeArquivo);
+    Estrada *E = getEstrada(nomeArquivo);
 
-    double vizinhanca[T->N];
+    double vizinhanca[E->N];
 
-    vizinhanca[0] = ((double)T->C[1].Posicao - T->C[0].Posicao) / 2;
-    vizinhanca[0] += T->C[0].Posicao;
+    vizinhanca[0] = ((double)E->C[1].Posicao - E->C[0].Posicao) / 2;
+    vizinhanca[0] += (double)E->C[0].Posicao;
 
     int i;
-    for(i = 1; i < T->N; i++){
-        if(i != T->N - 1){
-            vizinhanca[i] = ((double)T->C[i + 1].Posicao - T->C[i].Posicao) / 2;
-            vizinhanca[i] += ((double)T->C[i].Posicao - T->C[i - 1].Posicao) / 2;
+    for(i = 1; i < E->N; i++){
+        if(i != E->N - 1){
+            vizinhanca[i] = ((double)E->C[i + 1].Posicao - E->C[i].Posicao) / 2;
+            vizinhanca[i] += ((double)E->C[i].Posicao - E->C[i - 1].Posicao) / 2;
         }
         else{
-            vizinhanca[i] = ((double)T->C[i].Posicao - T->C [i - 1].Posicao) / 2;
-            vizinhanca[i] += ((double)T->T - T->C[i].Posicao);
+            vizinhanca[i] = ((double)E->C[i].Posicao - E->C [i - 1].Posicao) / 2;
+            vizinhanca[i] += ((double)E->T - E->C[i].Posicao);
         }
     }
 
     int j;
     double menor;
-    for(i = 0; i < T->N; i++){
+    for(i = 0; i < E->N; i++){
         for(j = 0; j < i; j++){
             if(vizinhanca[i] < vizinhanca[j]){
                 menor = vizinhanca[i];
@@ -100,6 +119,32 @@ double calcularMenorVizinhanca(const char *nomeArquivo){
     return menor;
 }
 
+char *cidadeMenorVizinhanca(const char *nomeArquivo){
+    Estrada *E = getEstrada(nomeArquivo);
+    
+    double vizinhanca[E->N];
 
+    vizinhanca[0] = ((double)E->C[1].Posicao - E->C[0].Posicao) / 2;
+    vizinhanca[0] += (double)E->C[0].Posicao;
 
+    int i;
+    for(i = 1; i < E->N; i++){
+        if(i != E->N - 1){
+            vizinhanca[i] = ((double)E->C[i + 1].Posicao - E->C[i].Posicao) / 2;
+            vizinhanca[i] += ((double)E->C[i].Posicao - E->C[i - 1].Posicao) / 2;
+        }
+        else{
+            vizinhanca[i] = ((double)E->C[i].Posicao - E->C [i - 1].Posicao) / 2;
+            vizinhanca[i] += ((double)E->T - E->C[i].Posicao);
+        }
+    }
+
+    double v = calcularMenorVizinhanca(nomeArquivo);
+    
+    for(i = 0; i < E->N; i++){
+        if(v == vizinhanca[i]){
+            return E->C[i].Nome;
+        }
+    }
+}
 
--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(){
 
     Estrada *T1 = getEstrada("Teste01.txt");
     double D1 = calcularMenorVizinhanca("Teste01.txt");
-    /* char *C1 = cidadeMenorVizinhanca("Teste01.txt"); */
+    char *C1 = cidadeMenorVizinhanca("Teste01.txt");
 
     if (T1->T == 10) Soma++;
     NumTeste++;
@@ -24,10 +24,8 @@ int main(){
     if (D1 == 3.5) Soma++;
     NumTeste++;
 
-/*     if (strcmp(C1, "Birnin Zana") == 0) Soma++;
-    NumTeste++; */
+    if (strcmp(C1, "Birnin Zana") == 0) Soma++;
+    NumTeste++;
 
     fprintf(Resposta, "\n\nATENÇÃO: Você acertou %d de %d itens. Logo, em 2.00 pontos, sua nota foi %.2f.\n", Soma, NumTeste, 2.0 * (float)Soma/(float)NumTeste);
-
-    printf("Tamanho estrada = %d , cidades = %d, cidade1 = %s, menor vizinhanca = %.2lf", T1->T, T1->N, T1->C[1].Nome, D1);
 }
